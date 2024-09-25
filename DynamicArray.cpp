128d29aab75f58b7fbe916b42b068e1250bb9dfe
Venugopal K <venu.shastri@gmail.com>
2024-09-25 12:03:58 +0530

    Update DynamicArray.cpp

--- DynamicArray.cpp
+++ DynamicArray.cpp
@@ -1,32 +1,32 @@
-#include<iostream> // Include the iostream library for input and output
-using namespace std; // Use the standard namespace
+#include<iostream>
+using namespace std;
 
-// Define a class named DynamicArray
+template<typename ItemType>
 class DynamicArray {
     private:
     int size; // Variable to store the size of the array
-    int* arr; // Pointer to dynamically allocated array
+    ItemType* arr; // Pointer to dynamically allocated array
     int index; // Variable to keep track of the current index
 
     public:
     // Parameterized constructor
     DynamicArray(int sizeArg): size{sizeArg}, index{0} {
-        this->arr = new int[size]; // Allocate memory for the array
+        this->arr = new ItemType[size]; // Allocate memory for the array
     }
 
     // Method to add an integer value to the array
-    void add(int intergerValue) {
+    void add(ItemType item) {
         if(this->index < this->size) {
-            this->arr[this->index] = intergerValue; // Add value to the array
+            this->arr[this->index] = item; // Add value to the array
             this->index++; // Increment the index
         } else {
             // If the array is full, create a new array with increased size
             size += 1;
-            int *newArr = new int[size]; // Allocate memory for the new array
+            ItemType *newArr = new ItemType[size]; // Allocate memory for the new array
             for(int i = 0; i < size - 1; i++) {
                 newArr[i] = arr[i]; // Copy elements to the new array
             }
-            newArr[size - 1] = intergerValue; // Add the new value to the new array
+            newArr[size - 1] = item; // Add the new value to the new array
             this->index++; // Increment the index
             delete[] arr; // Free the old array memory
             arr = newArr; // Point to the new array
@@ -40,12 +40,12 @@ class DynamicArray {
     }
 
     // Overload the subscript operator to access array elements
-    int operator[](int indexArg) {
+    ItemType operator[](int indexArg) {
         return this->arr[indexArg]; // Return the element at the given index
     }
-    friend std::ostream& operator<<(std::ostream& os, const DynamicArray& operand2);
+    friend std::ostream& operator<<(std::ostream& os, const DynamicArray<ItemType>& operand2);
 };
-std::ostream& operator<<(std::ostream& os, const DynamicArray& printobj){
+std::ostream& operator<<(std::ostream& os, const DynamicArray<ItemType>& printobj){
     for(int i = 0; i < printobj.size; i++) {
         os << printobj.arr[i] << " "; // Print each element
     }
@@ -53,19 +53,19 @@ std::ostream& operator<<(std::ostream& os, const DynamicArray& printobj){
 }
 // Main function
 int main() {
-    DynamicArray numbers(2); // Create a DynamicArray object with size 2
+    DynamicArray<int> numbers(2); // Create a DynamicArray object with size 2
     numbers.add(10); // Add 10 to the array
     numbers.add(20); // Add 20 to the array
     numbers.add(30); // Add 30 to the array
-    numbers.add(40); // Add 40 to the array
-    numbers.add(10); // Add 10 to the array
-    numbers.add(20); // Add 20 to the array
-    numbers.add(30); // Add 30 to the array
-    numbers.add(40); // Add 40 to the array
-
     // Access the element at index 1
     int value = numbers[1];
     cout<<numbers<<endl;
+
+    DynamicArray<float> floatNumbers(2); // Create a DynamicArray object with size 2
+    floatNumbers.add(10.10f); 
+    floatNumbers.add(20.30f); 
+    floatNumbers.add(30.50f); 
+  cout<<floatNumbers<<endl;
 
     return 0; // Return 0 to indicate successful execution
 }
